ebc8cbcf6e0e681e910b93c4dc183b939d3dd33f
oletu89 <betrunski89@gmail.com>
2020-02-21 13:18:17 +0100

    canvi 1

--- prova/src/prova/prova.java
+++ prova/src/prova/prova.java
@@ -1,5 +1,15 @@
-package prova;
-
 public class prova {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		System.out.println("Hola soc L'Alberto Alejos i tuu?¿?¿");
+
+
+	}
+
 }
+
